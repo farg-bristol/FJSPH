c62319cfce5886dcafbd7328db67dd26466cca0d
Jamie MacLeod <jammy4536@gmail.com>
2024-04-01 21:33:37 +0100

    Update binary IO function locations
    Move some IO functions from IPT to BinaryIO where they are more appropriate.

--- src/BinaryIO.cpp
+++ src/BinaryIO.cpp
@@ -2,6 +2,7 @@
 /*********        Created by Jamie MacLeod, University of Bristol               *************/
 
 #include "BinaryIO.h"
+#include <TECIO.h>
 #include <filesystem> //Requires C++17 standard, needed for restart functionality
 #include <chrono>
 #include <thread>
@@ -16,7 +17,7 @@ inline std::string space2underscore(std::string text)
 }
 
 /*************************************************************************/
-/*************************** BINARY OUTPUTS ******************************/
+/********************** BINARY OUTPUT FUNCTIONS **************************/
 /*************************************************************************/
 inline void add_file_aux_data(void* const& file, std::string const& name, 
 				std::string const& var)
@@ -201,7 +202,124 @@ inline void add_zone_aux_data(void* const& file, int32_t const& zone,
 	#endif
 }
 
+inline void Write_Real_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, 
+								int32_t const& start, int32_t const& size,
+								vector<real> const& varVec, string const& varName)
+{
+	int retval;
+	#if FOD == 1
+		retval =  tecZoneVarWriteDoubleValues(fileHandle, outputZone, varCount, 0, size, &varVec[start]);
+	#else
+		retval =  tecZoneVarWriteFloatValues(fileHandle, outputZone, varCount, 0, size, &varVec[start]);
+	#endif	
 
+	if(retval)
+	{
+		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
+		exit(-1);
+	}
+	varCount++;
+}
+
+
+inline void Write_State_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount,
+								int32_t const& start, int32_t const& size,
+								vector<StateVecD> const& varVec, string const& varName)
+{
+	vector<real> vec(size);
+	for(uint dim = 0; dim < SIMDIM; ++dim)
+	{
+		#pragma omp parallel for
+		for(int ii = start; ii < start+size; ++ii)
+			vec[ii-start] = varVec[ii][dim];
+
+		string name = varName + " " + std::to_string(dim);
+		Write_Real_Vector(fileHandle, outputZone, varCount, 0, size, vec, name);
+	}
+}
+
+inline void Write_Int_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, 
+								int32_t const& start, int32_t const& size,
+								vector<int32_t> const& varVec, string const& varName)
+{
+	if(tecZoneVarWriteInt32Values(fileHandle, outputZone, varCount, 0, size, &varVec[start]))
+	{
+		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
+		exit(-1);
+	}
+	varCount++;
+}
+
+inline void Write_UInt_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, 
+								int32_t const& start, int32_t const& size,
+								vector<uint8_t> const& varVec, string const& varName)
+{
+	if(tecZoneVarWriteUInt8Values(fileHandle, outputZone, varCount, 0, size, &varVec[start]))
+	{
+		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
+		exit(-1);
+	}
+	varCount++;
+}
+
+inline void Write_Real_Value(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, int32_t const& size,
+						real const& value, string const& varName)
+{
+	int retval;
+	vector<real> rvec(size,0.0);
+	rvec[0] = value;
+	#if FOD == 1
+		retval = tecZoneVarWriteDoubleValues(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
+	#else
+		retval = tecZoneVarWriteFloatValues(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
+	#endif	
+
+	if(retval)
+	{
+		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
+		exit(-1);
+	}
+	varCount++;
+}
+
+inline void Write_Int_Value(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, int32_t const& size,
+						int32_t const& value, string const& varName)
+{
+	int retval;
+	vector<int32_t> rvec(size,0.0);
+	rvec[0] = value;
+
+	retval = tecZoneVarWriteInt32Values(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
+
+	if(retval)
+	{
+		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
+		exit(-1);
+	}
+	varCount++;
+}
+
+inline void Write_UInt_Value(void* const& fileHandle, int32_t const& outputZone, 
+	int32_t& varCount, int32_t const& size,	uint8_t const& value, string const& varName)
+{
+	int retval;
+	vector<uint8_t> rvec(size,0.0);
+	rvec[0] = value;
+
+	retval = tecZoneVarWriteUInt8Values(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
+
+	if(retval)
+	{
+		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
+		exit(-1);
+	}
+	varCount++;
+}
+
+
+/*************************************************************************/
+/*************************** BINARY OUTPUTS ******************************/
+/*************************************************************************/
 inline void Write_Zone(SPHState const& pnp1, real const& rho0, real const& scale, std::vector<uint> const& outvar, 
 					size_t const& start, size_t const& end, void* const& fileHandle, int32_t const& outputZone)
 {
@@ -756,4 +874,354 @@ void Init_Binary_PLT(SIM &svar, FLUID const& fvar, AERO const& avar, string cons
 		str.clear();
 	}
 	
+}
+
+void close_file(void* handle)
+{
+	if(tecFileWriterClose(&handle))
+		printf("Failed to close szplt file.\n");
+}
+
+void flush_file(void* handle)
+{
+	if(tecFileWriterFlush(handle,0,NULL))
+	{
+		cout << "Failed to flush data. Retrying..." << endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		// retry the flush
+		if(tecFileWriterFlush(handle,0,NULL))
+		{
+			cerr << "Failed to flush data to szplt file, stopping." << endl;
+			exit(-1);
+		}
+	}
+}
+
+
+/*************************************************************************/
+/************************* IPT BINARY OUTPUTS ****************************/
+/*************************************************************************/
+namespace IPT
+{
+    namespace BINARY
+    {
+        string Get_Variables(int const& offset)
+        {
+            string variables;
+                
+            if(offset == 1)
+                variables = "Y,Z";
+            else if(offset == 2)
+                variables = "X,Z";
+            else if (offset == 3)
+                variables = "X,Y";
+            else
+                variables = "X,Y,Z";
+
+            variables += 
+            ",t,dt,v,a,ptID,Cell_V,Cell_Rho,Cell_ID";
+            return variables;
+        }
+
+        string Cell_Variables(int const& offset)
+        {
+            string variables;
+                
+            if(offset == 1)
+                variables = "Y,Z";
+            else if(offset == 2)
+                variables = "X,Z";
+            else if (offset == 3)
+                variables = "X,Y";
+            else
+                variables = "X,Y,Z";
+
+            return variables;
+        }
+
+        void Open_File(string const& file, string const& title, string const& var, int32_t const& fileType, void* &fileHandle)
+        {
+            if(tecFileWriterOpen(file.c_str(),title.c_str(),var.c_str(),fileType,0,1,NULL,&fileHandle))
+            {
+                cout << "Failed to open " << file << endl;
+                exit(-1);
+            }
+            #ifdef DEBUG
+            if(tecFileSetDiagnosticsLevel(fileHandle, 1))
+            {
+                cerr << "Failed to set debug option for output file: " << file << endl;
+                exit(-1);
+            }
+            #endif
+        }
+
+        void Init_IPT_Files(SIM& svar)
+        {
+            string partf, cellf, streakf, surfacef;
+
+            partf = svar.output_prefix + "_IPT_scatter.szplt";
+
+            streakf = svar.output_prefix + "_streaks.szplt";
+
+            cellf = svar.output_prefix + "_cells.plt";
+
+            surfacef = svar.output_prefix + "_surface_impacts.szplt";
+
+            // int32_t fileType   = 0; /*0 = Full, 1 = Grid, 2 = Solution*/
+            // int32_t fileFormat = 1; // 0 == PLT, 1 == SZPLT
+            string part_variables = Get_Variables(svar.offset_axis);
+            string cell_variables = Cell_Variables(svar.offset_axis);
+
+            if(svar.partout == 1)
+            {
+                string title = "IPT particle scatter data";
+                Open_File(partf,title,part_variables,1,svar.partHandle);
+            }
+
+            if(svar.streakout == 1)
+            {
+                string title = "IPT particle streak data";
+                Open_File(streakf,title,part_variables,1,svar.streakHandle);
+            }
+
+            if(svar.cellsout == 1)
+            {
+                string title = "IPT particle cell intersection data";
+                Open_File(cellf,title,cell_variables,0,svar.cellHandle);
+            }
+        }
+
+        void Write_Point(SIM const& svar, IPTPart const& pnp1)
+        {
+            int64_t const size = 1;
+
+            double solTime = pnp1.t;     
+            int32_t outputZone;
+
+            #if SIMDIM == 3
+            vector<int32_t> varTypes = {realType,realType,realType,realType,3,realType,realType,3};
+            #else
+            vector<int32_t> varTypes = {realType,realType,realType,3,realType,realType,3};
+            #endif
+            vector<int32_t> shareVarFromZone(varTypes.size(),0);
+            vector<int32_t> valueLocation(varTypes.size(),1);
+            vector<int32_t> passiveVarList(varTypes.size(),0);
+ 
+            string group = "IPT Particle " + std::to_string(pnp1.partID) + " scatter data";
+            if(tecZoneCreateIJK(svar.partHandle,group.c_str(),size,1,1,&varTypes[0],
+                &shareVarFromZone[0],&valueLocation[0],&passiveVarList[0],0,0,0,&outputZone))
+            {
+                cerr << "Failed to create IJK zone." << endl;
+                exit(-1);
+            }
+
+            if(tecZoneSetUnsteadyOptions(svar.partHandle, outputZone, solTime, 4))
+            {
+                cerr << "Failed to add unsteady options." << endl;
+                exit(-1);
+            }
+
+            vector<real> x(size);
+            int32_t var = 1;
+
+            for(uint dim = 0; dim < SIMDIM; ++dim)
+            {
+                x[0] = pnp1.xi(dim)/svar.scale;
+
+                string name = "position coordinate ";
+                name.append(std::to_string(dim));
+                Write_Real_Vector(svar.partHandle, outputZone, var, 0, size, x, name);
+            }
+
+            vector<real> t(size);
+            vector<real> dt(size);
+            vector<real> vel(size);
+            vector<real> acc(size);
+            vector<real> cVel(size);
+            vector<real> cRho(size);
+            vector<int> pID(size);
+            vector<int> cID(size);
+
+            t[0] = pnp1.t;
+            dt[0] = pnp1.dt;
+            vel[0] = pnp1.v.norm();
+            acc[0] = pnp1.acc;
+            cVel[0] = pnp1.cellV.norm();
+            cRho[0] = pnp1.cellRho;
+            pID[0] = pnp1.partID;
+            cID[0] = pnp1.cellID;
+
+            Write_Real_Vector(svar.partHandle, outputZone, var, 0, size, t, "particle time");
+            Write_Real_Vector(svar.partHandle, outputZone, var, 0, size, dt, "timestep");
+            Write_Real_Vector(svar.partHandle, outputZone, var, 0, size, vel, "velocity magnitude");
+            Write_Real_Vector(svar.partHandle, outputZone, var, 0, size, acc, "acceleration magnitude");
+            Write_Int_Vector(svar.partHandle, outputZone, var, 0, size, pID, "particle ID");
+			Write_Real_Vector(svar.partHandle, outputZone, var, 0, size, cVel, "cell velocity");
+			Write_Real_Vector(svar.partHandle, outputZone, var, 0, size, cRho, "cell density");
+			Write_Int_Vector(svar.partHandle, outputZone, var, 0, size, cID, "cell ID");
+
+            if(tecFileWriterFlush(svar.partHandle,0,NULL))
+            {
+                cout << "Failed to flush data. Retrying..." << endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                // retry the flush
+                if(tecFileWriterFlush(svar.partHandle,0,NULL))
+                {
+                    cerr << "Failed to flush data to particle file" << endl;
+                    exit(-1);
+                }
+            }
+        }
+
+        void Write_State(SIM const& svar, IPTState const& pnp1, string const& zoneName, void* &fileHandle)
+        {
+            int64_t const size = pnp1.size();
+
+            double solTime = svar.t;     
+            int32_t outputZone;
+
+            #if SIMDIM == 3    
+            // variables =  "X,Y,Z,t,dt,v,a,ptID,Cell_V,Cell_Rho,Cell_ID"
+            vector<int32_t> varTypes = 
+            {realType,realType,realType,realType,realType,realType,realType,3,realType,realType,3};
+            #else
+            // variables =  "X,Z,t,dt,v,a,ptID,Cell_V,Cell_Rho,Cell_ID"
+            vector<int32_t> varTypes = 
+                {realType,realType,realType,realType,realType,realType,3,realType,realType,3};
+            #endif
+            vector<int32_t> shareVarFromZone(varTypes.size(),0);
+            vector<int32_t> valueLocation(varTypes.size(),1);
+            vector<int32_t> passiveVarList(varTypes.size(),0);
+ 
+            if(tecZoneCreateIJK(fileHandle,zoneName.c_str(),size,1,1,&varTypes[0],
+                &shareVarFromZone[0],&valueLocation[0],&passiveVarList[0],0,0,0,&outputZone))
+            {
+                cerr << "Failed to create IJK zone." << endl;
+                exit(-1);
+            }
+
+            if(tecZoneSetUnsteadyOptions(fileHandle, outputZone, solTime, 6))
+            {
+                cerr << "Failed to add unsteady options." << endl;
+                exit(-1);
+            }
+
+            vector<real> x(size);
+            int32_t var = 1;
+
+            for(uint dim = 0; dim < SIMDIM; ++dim)
+            {
+                #pragma omp parallel for
+                for(uint ii = 0; ii < size; ++ii)
+                    x[ii] = pnp1[ii].xi(dim)/svar.scale;
+
+                string name = "position coordinate " + std::to_string(dim);
+                Write_Real_Vector(fileHandle, outputZone, var, 0, size, x, name);
+            }
+
+            vector<real> t(size);
+            vector<real> dt(size);
+            vector<real> vel(size);
+            vector<real> acc(size);
+            vector<real> cVel(size);
+            vector<real> cRho(size);
+            vector<int> pID(size);
+            vector<int> cID(size);
+
+            for (int ii = 0; ii < size; ++ii)
+            {
+                t[ii] = pnp1[ii].t;
+                dt[ii] = pnp1[ii].dt;
+                vel[ii] = pnp1[ii].v.norm();
+                acc[ii] = pnp1[ii].acc;
+                cVel[ii] = pnp1[ii].cellV.norm();
+                cRho[ii] = pnp1[ii].cellRho;
+                pID[ii] = pnp1[ii].partID;
+                cID[ii] = pnp1[ii].cellID;
+            }
+
+            Write_Real_Vector(fileHandle, outputZone, var, 0, size, t, "particle time");
+            Write_Real_Vector(fileHandle, outputZone, var, 0, size, dt, "timestep");
+            Write_Real_Vector(fileHandle, outputZone, var, 0, size, vel, "velocity magnitude");
+            Write_Real_Vector(fileHandle, outputZone, var, 0, size, acc, "acceleration magnitude");
+            Write_Int_Vector(fileHandle, outputZone, var, 0, size, pID, "particle ID");
+			Write_Real_Vector(fileHandle, outputZone, var, 0, size, cVel, "cell velocity");
+			Write_Real_Vector(fileHandle, outputZone, var, 0, size, cRho, "cell density");
+			Write_Int_Vector(fileHandle, outputZone, var, 0, size, cID, "cell ID");
+
+        }
+
+        void Write_Cells(SIM& svar, MESH const& cells, IPTState const& pnp1, int32_t const& totalNumFaceNodes, 
+            vector<StateVecD> const& usedVerts, vector<int32_t> const& cellIndexes,
+            vector<vector<int32_t>> const& faces, vector<int32_t> const& left, vector<int32_t> const& right)
+        {
+            // int64_t const size = pnp1.size();
+
+            double solTime = svar.t;     
+            int32_t outputZone;
+
+            #if SIMDIM == 3
+            vector<int32_t> varTypes = {realType,realType,realType,realType,3,realType,realType,3};
+            #else
+            vector<int32_t> varTypes = {realType,realType,realType,3,realType,realType,3};
+            #endif
+            vector<int32_t> shareVarFromZone(varTypes.size(),0);
+            vector<int32_t> valueLocation(varTypes.size(),1);
+            vector<int32_t> passiveVarList(varTypes.size(),0);
+
+            #if SIMDIM == 3
+            int32_t zoneType = 7; /* FE Polyhedron */
+            #else
+            int32_t zoneType = 6; /* FE Polygon */
+            #endif
+            int32_t nNodes = usedVerts.size();
+            int32_t nFaces = faces.size();
+            int32_t nCells = cellIndexes.size();
+
+            string group = "IPT cell intersection data";
+            if(tecZoneCreatePoly(svar.cellHandle,group.c_str(),zoneType,nNodes,nFaces,nCells,
+                totalNumFaceNodes,&varTypes[0],&shareVarFromZone[0],&valueLocation[0],
+                &passiveVarList[0],0,0,0,&outputZone))
+            {
+                cerr << "Failed to create polyhedral/polygonal zone." << endl;
+                exit(-1);
+            }
+
+            if(tecZoneSetUnsteadyOptions(svar.cellHandle, outputZone, solTime, 7))
+            {
+                cerr << "Failed to add unsteady options." << endl;
+                exit(-1);
+            }
+
+            vector<real> x(nNodes);
+            int32_t var = 1;
+
+            for(uint dim = 0; dim < SIMDIM; ++dim)
+            {
+                #pragma omp parallel for
+                for(int32_t ii = 0; ii < nNodes; ++ii)
+                    x[ii] = usedVerts[ii][dim];
+
+                string name = "position coordinate " + std::to_string(dim);
+                Write_Real_Vector(svar.cellHandle, outputZone, var, 0, nNodes, x, name);
+            }
+
+            vector<int32_t> faceCounts(faces.size());
+            vector<int32_t> faceNodes(totalNumFaceNodes);
+            size_t jj = 0;
+            /*Inform of how many vertices in each face*/
+            for (size_t ii = 0; ii < faces.size(); ++ii)
+            {
+                faceCounts[ii] =  faces[ii].size();
+                for(auto const& vertex:faces[ii])
+                {	/*Write face vertex indexes*/
+                    faceNodes[jj] = vertex;
+                    jj++;
+                }
+            }
+
+            tecZoneWritePolyFaces32(svar.cellHandle,outputZone,0,nFaces,&faceCounts[0],
+                            &faceNodes[0],&left[0],&right[0],1);
+        }
+    }
 }
--- src/BinaryIO.h
+++ src/BinaryIO.h
@@ -5,7 +5,6 @@
 #define BINARYIO_H
 
 #include "Var.h"
-#include <TECIO.h>
 
 inline int Combine_SZPLT(string& file)
 {
@@ -28,127 +27,30 @@ inline int Combine_SZPLT(string& file)
 	return 0;
 }
 
-/*************************************************************************/
-/*************************** BINARY OUTPUTS ******************************/
-/*************************************************************************/
-inline void Write_Real_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, 
-								int32_t const& start, int32_t const& size,
-								vector<real> const& varVec, string const& varName)
-{
-	int retval;
-	#if FOD == 1
-		retval =  tecZoneVarWriteDoubleValues(fileHandle, outputZone, varCount, 0, size, &varVec[start]);
-	#else
-		retval =  tecZoneVarWriteFloatValues(fileHandle, outputZone, varCount, 0, size, &varVec[start]);
-	#endif	
-
-	if(retval)
-	{
-		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
-		exit(-1);
-	}
-	varCount++;
-}
-
-
-inline void Write_State_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount,
-								int32_t const& start, int32_t const& size,
-								vector<StateVecD> const& varVec, string const& varName)
-{
-	vector<real> vec(size);
-	for(uint dim = 0; dim < SIMDIM; ++dim)
-	{
-		#pragma omp parallel for
-		for(int ii = start; ii < start+size; ++ii)
-			vec[ii-start] = varVec[ii][dim];
-
-		string name = varName + " " + std::to_string(dim);
-		Write_Real_Vector(fileHandle, outputZone, varCount, 0, size, vec, name);
-	}
-}
-
-inline void Write_Int_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, 
-								int32_t const& start, int32_t const& size,
-								vector<int32_t> const& varVec, string const& varName)
-{
-	if(tecZoneVarWriteInt32Values(fileHandle, outputZone, varCount, 0, size, &varVec[start]))
-	{
-		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
-		exit(-1);
-	}
-	varCount++;
-}
-
-inline void Write_UInt_Vector(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, 
-								int32_t const& start, int32_t const& size,
-								vector<uint8_t> const& varVec, string const& varName)
-{
-	if(tecZoneVarWriteUInt8Values(fileHandle, outputZone, varCount, 0, size, &varVec[start]))
-	{
-		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
-		exit(-1);
-	}
-	varCount++;
-}
-
-inline void Write_Real_Value(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, int32_t const& size,
-						real const& value, string const& varName)
-{
-	int retval;
-	vector<real> rvec(size,0.0);
-	rvec[0] = value;
-	#if FOD == 1
-		retval = tecZoneVarWriteDoubleValues(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
-	#else
-		retval = tecZoneVarWriteFloatValues(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
-	#endif	
-
-	if(retval)
-	{
-		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
-		exit(-1);
-	}
-	varCount++;
-}
-
-inline void Write_Int_Value(void* const& fileHandle, int32_t const& outputZone, int32_t& varCount, int32_t const& size,
-						int32_t const& value, string const& varName)
-{
-	int retval;
-	vector<int32_t> rvec(size,0.0);
-	rvec[0] = value;
-
-	retval = tecZoneVarWriteInt32Values(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
-
-	if(retval)
-	{
-		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
-		exit(-1);
-	}
-	varCount++;
-}
-
-inline void Write_UInt_Value(void* const& fileHandle, int32_t const& outputZone, 
-	int32_t& varCount, int32_t const& size,	uint8_t const& value, string const& varName)
-{
-	int retval;
-	vector<uint8_t> rvec(size,0.0);
-	rvec[0] = value;
-
-	retval = tecZoneVarWriteUInt8Values(fileHandle, outputZone, varCount, 0, size, &rvec[0]);
-
-	if(retval)
-	{
-		printf("Failed to write \"%s\". zone: %d. varCount: %d\n", varName.c_str(), outputZone, varCount);
-		exit(-1);
-	}
-	varCount++;
-}
-
 void Write_Binary_Timestep(SIM const& svar, real const& rho0, SPHState const& pnp1,
 	bound_block const& limits, char const* group, int32_t const& strandID, void* const& fileHandle);
 
 void Init_Binary_PLT(SIM &svar, FLUID const& fvar, AERO const& avar, string const& prefix,
 			 string const& filename, string const& zoneName, void* &fileHandle);
 
+void close_file(void* handle);
+
+void flush_file(void* handle);
+
+// IPT Binary functions (Allows more explicit library includes, so that tecio is only in BinaryIO.cpp.)
+namespace IPT
+{
+	namespace BINARY
+	{
+        void Init_IPT_Files(SIM& svar);
+
+		void Write_Point(SIM const& svar, IPTPart const& pnp1);
+
+        void Write_State(SIM const& svar, IPTState const& pnp1, string const& zoneName, void* &fileHandle);
+
+		void Write_Cells(SIM& svar, MESH const& cells, IPTState const& pnp1, int32_t const& totalNumFaceNodes, 
+            vector<StateVecD> const& usedVerts, vector<int32_t> const& cellIndexes,
+            vector<vector<int32_t>> const& faces, vector<int32_t> const& left, vector<int32_t> const& right);
+	}
+}
 #endif
